dbb3197e6ed7169d399043e675c46052d41c79fc
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2021-09-08 15:08:23 +0530

    Set logger level to INFO to get SQL Queries in output and updated code to clean the cache.

--- jpa-demo1/src/com/mahendra/Main1.java
+++ jpa-demo1/src/com/mahendra/Main1.java
@@ -9,20 +9,32 @@ import javax.persistence.Persistence;
 public class Main1 {
 
 	public static void main(String[] args) {
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA-PU");
 		EntityManager em = emf.createEntityManager();
-		
-		// Connect to DB, Create Statement, execute statement, get results, convert all records into LIST of objects
+
+		// Connect to DB, Create Statement, execute statement, get results, convert all
+		// records into LIST of objects
 		// close the connection!!
 		List<Employee> employees = em.createQuery("select e from Employee e").setMaxResults(100).getResultList();
-		
-		System.out.println("Found records: "+employees.size());
-		System.out.printf("%10s | %-15s | %-16s \n","EMPLOYEE ID","FIRST NAME","LAST NAME");
-		for(Employee e: employees) {
-			System.out.printf("%10s | %-15s | %-16s | %s\n",e.getEmpId(),e.getFirstName(),e.getLastName(),e.getBirthDate());
+
+		System.out.println("Found records: " + employees.size());
+		System.out.printf("%10s | %-15s | %-16s \n", "EMPLOYEE ID", "FIRST NAME", "LAST NAME");
+		for (Employee e : employees) {
+			System.out.printf("%10s | %-15s | %-16s | %s\n", e.getEmpId(), e.getFirstName(), e.getLastName(),
+					e.getBirthDate());
 		}
-		
+
+		Employee emp1 = em.find(Employee.class, 10003);
+		System.out.println("Is emp1 there in cache? " + em.contains(emp1));
+		System.out.println("Employee of the month " + emp1.getFirstName());
+
+		// remove from cache
+		em.clear();
+
+		System.out.println("After em.clear() is emp1 in cache now ?" + em.contains(emp1));
+
+		// Closing Persistence Context
 		em.close();
 
 	}
